e96588385f66ef39cfd51e46c5324106fcc3a2c1
ziemowitpl <roninrallegro@gmail.com>
2017-09-24 23:28:38 +0200

    Można określić liczbę spacji i je wyświetlić w komunikacie -010-

---
+++ 03 lancuchy znakow listy krotki i mapy/11 listy str33.py
@@ -0,0 +1,18 @@
+listaCzarodzieja = 'nogi pająka, żabi palec, oko traszki, skrzydło nietoperza, masło z pomrowika, łupież węża'
+print(listaCzarodzieja)
+print('to był łańcuch')
+print('--------')
+listaCzarodzieja2 = ['nogi pająka', 'żabi palec', 'oko traszki', 'skrzydło nietoperza', 'masło z pomrowika', 'łupież węża']
+print(listaCzarodzieja2)
+print('to była lista')
+print('--------')
+print('Przykład z listą:')
+print(listaCzarodzieja2[2])
+print(listaCzarodzieja2[0])
+print(listaCzarodzieja2[1])
+print('--------')
+listaCzarodzieja2[2] = 'język ślimaka'
+print(listaCzarodzieja2)
+print('Zmiana elementu listy')
+print('--------')
+print('Lista zaczyna się od ZERA a nie od JEDEN')
